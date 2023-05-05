9a4744c70c4d0ddf33f8468d1b940d23fb34d289
SwansonSays <rpswanson18@gmail.com>
2023-05-05 16:17:06 -0700

    added shm test file

--- shm_test/lidar_test.c
+++ shm_test/lidar_test.c
@@ -12,7 +12,7 @@ struct lidar_data {
 int main(int argc, char *argv[]) {
     const char* name = "SHARED_MEMORY";
     int shm_fd = shm_open(name, O_RDONLY, 0666);
-    struct lidar_data* data = (lidar_data*)mmap(NULL, sizeof(*data), PROT_READ, MAP_SHARED, shm_fd, 0);
+    struct lidar_data *data = (lidar_data*)mmap(NULL, sizeof(*data), PROT_READ, MAP_SHARED, shm_fd, 0);
 
     while (1) {
         printf("THETA [%f] | DISTANCE [%f] | QUALITY [%d]\n", data->theta, data->distance, data->quality);
